5cf7a073a0f73d4f14075c8e155d2c49ec5d5ece
Tej eswar <eswar@gmail.com>
2024-12-07 10:58:21 +0530

    final

--- src/main/java/com/klef/jfsd/springboot/controller/ClientController.java
+++ src/main/java/com/klef/jfsd/springboot/controller/ClientController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @RestController
 public class ClientController {
 
@@ -15,27 +14,27 @@ public class ClientController {
 	public String getMethodName() {
 		return "Welcome to JFSD EndSemLab";
 	}
-	
+
 	@GetMapping("add")
 	public String demo1(@RequestParam("p") int x, @RequestParam("q") int y) {
 		int r = x + y;
-		
+
 		return Integer.toString(r);
 	}
-	
+
 	@GetMapping("display/{fname}/{lname}")
 	@ResponseBody
 	public String display(@PathVariable("fname") String a, @PathVariable("lname") String b) {
 		String c = a + " " + b;
 		return (c);
 	}
+
 	@GetMapping("employeehome")
-    public ModelAndView employee() {
+	public ModelAndView employee() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("employee");
 		return mv;
 	}
-	
 
 	@GetMapping("displayname/{num1}/{lname}")
 	@ResponseBody
@@ -43,13 +42,29 @@ public class ClientController {
 		String c = a + " " + b;
 		return (c);
 	}
-	
-	
+
 	@GetMapping("mul")
 	public String demo6(@RequestParam("p") int x, @RequestParam("q") int y) {
 		int r = x * y;
 		return Integer.toString(r);
 	}
+
+	@GetMapping("multonew")
 	
+	public ModelAndView multonew(@RequestParam("x") int a, @RequestParam("y") int b) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("output");	
+		mv.addObject("result", a * b);
+		return mv;
+	}
 	
+	@GetMapping("name/{num1}/{lname}")
+	@ResponseBody
+	public ModelAndView name(@PathVariable("num1") String a, @PathVariable("lname") String b) {
+		ModelAndView mv = new ModelAndView();
+		String c = a + " " + b;
+		mv.setViewName("nameoutput");	
+		mv.addObject("name", c);
+		return mv;
+	}
 }
